583ce4fdc57f4713e322ceb588a38c932f7d21a3
dong3789 <dhlovey@gmail.com>
2020-02-17 10:53:51 +0900

    addMsg

--- src/message/model/vo/Message.java
+++ src/message/model/vo/Message.java
@@ -10,13 +10,12 @@ public class Message {
 	private String rsgId;
 	private String ssgDel;
 	private String rsgDel;
-	
+	private String status;
 	
 	public Message() {}
 
-
-	public Message(int msgNum, String msgTitle, Date msgDate, String ssgId, String rsgId, String ssgDel,
-			String rsgDel) {
+	public Message(int msgNum, String msgTitle, Date msgDate, String ssgId, String rsgId, String ssgDel, String rsgDel,
+			String status) {
 		super();
 		this.msgNum = msgNum;
 		this.msgTitle = msgTitle;
@@ -25,78 +24,75 @@ public class Message {
 		this.rsgId = rsgId;
 		this.ssgDel = ssgDel;
 		this.rsgDel = rsgDel;
+		this.status = status;
 	}
-
-
-
 
 	public int getMsgNum() {
 		return msgNum;
 	}
 
-
 	public void setMsgNum(int msgNum) {
 		this.msgNum = msgNum;
 	}
-
 
 	public String getMsgTitle() {
 		return msgTitle;
 	}
 
-
 	public void setMsgTitle(String msgTitle) {
 		this.msgTitle = msgTitle;
 	}
-
 
 	public Date getMsgDate() {
 		return msgDate;
 	}
 
-
 	public void setMsgDate(Date msgDate) {
 		this.msgDate = msgDate;
 	}
-
 
 	public String getSsgId() {
 		return ssgId;
 	}
 
-
 	public void setSsgId(String ssgId) {
 		this.ssgId = ssgId;
 	}
-
 
 	public String getRsgId() {
 		return rsgId;
 	}
 
-
 	public void setRsgId(String rsgId) {
 		this.rsgId = rsgId;
 	}
-
 
 	public String getSsgDel() {
 		return ssgDel;
 	}
 
-
 	public void setSsgDel(String ssgDel) {
 		this.ssgDel = ssgDel;
 	}
-
 
 	public String getRsgDel() {
 		return rsgDel;
 	}
 
-
 	public void setRsgDel(String rsgDel) {
 		this.rsgDel = rsgDel;
-	};
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 	
 }
+
+
