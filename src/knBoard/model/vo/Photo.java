e183ee8d8bf44148e9707ed36ae1cdedd25e008f
user1 <user1@DESKTOP-TTPQKH9>
2020-02-17 10:52:24 +0900

    field

--- src/knBoard/model/vo/Photo.java
+++ src/knBoard/model/vo/Photo.java
@@ -14,4 +14,20 @@ public class Photo {
 	private String phDel;
 	
 	public Photo() {}
+
+	public Photo(int phNum, String phOrig, String phChng, String phPath, Date phUpload, int phFnum, int rvNum,
+			int knNum, String phDel) {
+		super();
+		this.phNum = phNum;
+		this.phOrig = phOrig;
+		this.phChng = phChng;
+		this.phPath = phPath;
+		this.phUpload = phUpload;
+		this.phFnum = phFnum;
+		this.rvNum = rvNum;
+		this.knNum = knNum;
+		this.phDel = phDel;
+	}
+	
+	
 }
